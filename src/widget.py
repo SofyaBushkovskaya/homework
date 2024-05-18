4dcd2b0ac97177d4dfc3c05ae0d5b377e89aa33c
Sofya Bushkovskaya <sonya.yudina17@mail.ru>
2024-05-19 00:23:12 +0500

    create branch develop and feature/homework_10_1

---
+++ src/widget.py
@@ -0,0 +1,21 @@
+from src.masks import get_mask_account, get_mask_card
+
+
+def get_mask_account_card(number: str) -> str:
+    """Функция принимает строки и маскирует номер карты или счёта"""
+    if len(number) > 0:
+        if len(number.split()[-1]) == 16:
+            new_number = get_mask_card(number.split()[-1])
+            result = f"{number[:-16]}{new_number}"
+            return result
+        elif len(number.split()[-1]) == 20:
+            new_number = get_mask_account(number.split()[-1])
+            result = f"{number[:-20]}{new_number}"
+            return result
+    return ""
+
+
+def get_new_data(old_data: str) -> str:
+    """Функция принимает строку с датой и форматирует её"""
+    data_slise = old_data[0:10].split("-")
+    return ".".join(data_slise[::-1])
