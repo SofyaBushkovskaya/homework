15f0041c1b79e3dd382871285bc7ba2dd927425b
Sofya Bushkovskaya <sonya.yudina17@mail.ru>
2024-05-19 01:20:04 +0500

    create processing.py

--- src/processing.py
+++ src/processing.py
@@ -8,3 +8,12 @@ def filter_by_state(transactions: list, state: str = "EXECUTED") -> list:
         if transaction.get("state") == state:
             filter_transactions.append(transaction)
     return filter_transactions
+
+
+def sort_by_date(transactions: list, order: bool = True) -> list:
+    """
+    Функция использует встроенную функцию sorted() для сортировки списка словарей по дате
+    """
+    # Сортировка по дате с использованием лямбда-функции
+    sorted_transactions = sorted(transactions, key=lambda x: x["date"], reverse=order)
+    return sorted_transactions
