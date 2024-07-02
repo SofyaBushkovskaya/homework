798be24cd56fa1899762144b8ff7aa8796ca2b37
Sofya Bushkovskaya <sonya.yudina17@mail.ru>
2024-07-02 23:41:16 +0500

    add main funcs

--- src/main.py
+++ src/main.py
@@ -1,11 +1,16 @@
-import os.path
+import os
 import pprint
 
+import pandas as pd
 from src.external_api import currency_conversion
 from src.generators import card_number_generator, filter_by_currency, transaction_descriptions
 from src.processing import filter_by_state, sort_by_date
 from src.utils import financial_transactions
 from src.widget import get_mask_account_card, get_new_data
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+path_to_csv = os.path.abspath("../data/transactions.csv")
+path_to_xlsx = os.path.abspath("../data/transactions_excel.xlsx")
 
 print(get_mask_account_card("MasterCard 7158300734726758"))
 
@@ -124,3 +129,24 @@ transactions1 = financial_transactions(file_path)
 for transaction in transactions1:
     rub_amount = currency_conversion(transaction)
     print(f"Transaction amount in RUB: {rub_amount}")
+
+
+def get_transaction_csv_data(path_to_csv):
+    """Функция принимает путь до файла CSV и возвращает данные транзакций"""
+    with open(path_to_csv, "r") as file:
+        data = pd.read_csv(file)
+        return data
+
+
+def get_transaction_xlsx_data(path_to_xlsx):
+    """Функция принимает путь до файла XLSX и возвращает данные транзакций"""
+    with open(path_to_xlsx, "rb") as file:
+        data = pd.read_excel(file)
+        return data
+
+
+if __name__ == "__main__":
+    data = get_transaction_csv_data(path_to_csv)
+    print(data.head())
+    data = get_transaction_xlsx_data(path_to_xlsx)
+    print(data.head())
